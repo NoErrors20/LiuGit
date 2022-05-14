be849359a32bcea118fb801941241f216ba697a0
Rem <rem@gmail.com>
2022-05-14 12:11:53 +0800

    刘正阳第一次提交--test

---
+++ src/main/java/com/example/liugit/LiuGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.liugit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiuGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiuGitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/liugit/LiuGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.liugit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiuGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
